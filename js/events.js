02380fe7d2b1c8c3398e0a5a38e30fab4c22ebe4
Sasha <sanechegg@gmail.com>
2016-01-29 19:31:46 +0200

    Create events.js

---
+++ js/events.js
@@ -0,0 +1,24 @@
+{
+	"01": [
+		{
+			"name" : "Grandmothers birthday",
+			"place" : "city Volshansk"
+		}
+	],
+	"15": [
+		{
+			"name" : "Task defend",
+			"place" : "EPAM office."
+		},
+		{
+			"name" : "Meeting with friend",
+			"place" : "Pub. Pushkinska street, 9."
+		}
+	],
+	"20" : [
+		{
+			"name" : "Andreys birthday",
+			"place" : "His home."
+		}
+	]
+}
